8f889f11b7d37608c24a52a79eee4acfacd2f518
ldjorge1988@gmail.com <ldjorge1988@gmail.com>
2022-05-11 23:13:27 -0500

    create proyect

---
+++ src/main/java/co/edu/uco/transport/infrastructure/transportinfrastructuremaven/TransportInfrastructureMavenApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.transport.infrastructure.transportinfrastructuremaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportInfrastructureMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportInfrastructureMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/transport/infrastructure/transportinfrastructuremaven/TransportInfrastructureMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.transport.infrastructure.transportinfrastructuremaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportInfrastructureMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
